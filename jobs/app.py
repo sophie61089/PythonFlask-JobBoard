b8876ebf2af937e3c778479c54217b0d25deef72
sophie61089 <44974945+sophie61089@users.noreply.github.com>
2019-02-08 10:17:52 +0000

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -51,7 +51,7 @@ def employer(employer_id):
     jobs= execute_sql("SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?",
                       [employer_id])
     reviews= execute_sql("SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?",
-                         [employer_id])
+                         [employer_id] )
     return render_template("employer.html", employer=employer, jobs=jobs, reviews=reviews)
 
 
